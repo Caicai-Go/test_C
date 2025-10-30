46fd442b7a0884790da2ec19428176c03d7206a9
juncai---programming <1872192985@qq.com>
2025-10-30 23:21:03 +0800

    大到小输出

---
+++ From_big_to_small/From_big_to_small/test.c
@@ -0,0 +1,41 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+//大到小输出
+
+#include <stdio.h>
+
+int main() {
+	int a = 0;
+	int b = 0;
+	int c = 0;
+	scanf("%d %d %d", &a, &b, &c);
+	if (a > b)
+	{
+		if (a > c)
+		{
+			if (b > c)
+			{
+				printf("%d %d %d\n", a, b, c);
+			}
+			else
+			{
+				printf("%d %d %d\n", a, c, b);
+			}
+		}
+		else {
+			printf("%d %d %d\n", c,a,b);
+		}
+	}
+	else if (b > c)
+	{
+		if (a > c)
+		{
+			printf("%d %d %d\n", b,a,c);
+		}
+		else {
+			printf("%d %d %d\n", b,c,a);
+		}
+	}
+	else {
+		printf("%d %d %d\n", c, b, a);
+	}
+}
